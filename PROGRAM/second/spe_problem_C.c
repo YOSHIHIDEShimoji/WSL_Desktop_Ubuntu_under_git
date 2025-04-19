f61317056df7a79fe14c8d9bac52c4703e780133
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-04-19 16:12:28 +0900

    finish second problem

---
+++ PROGRAM/second/spe_problem_C.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("文字Aの値 = %d\n", 'A');
+	printf("8進数の100 = %d\n", 0100);
+	printf("10進数の100 = %d\n", 100);
+	printf("16進数の100 = %d\n", 0x100);
+	return 0;
+}
