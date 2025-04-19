7ba078acaeeb93019225c9ad563a6ec5b4beb0c1
YOSHIHIDEShimoji <g.y.shimoji@gmail.com>
2025-04-19 15:25:25 +0900

    finish B problem

---
+++ PROGRAM/second/spe_problem_A.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	double x;
+	for(x = 1; x <= 7; x++){
+		printf("6 / %f = %f\n", x, 6 / x);
+	}
+	
+	return 0;
+}
+
---
+++ PROGRAM/second/spe_problem_B.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+	char c = 127;
+	printf("c = %d\n", c);
+
+	c = c + 1;
+	printf("c = %d\n", c);
+}
